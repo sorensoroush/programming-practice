339a55b2b258a3d256f888054c540b44f8bed0cb
Soren <sorensoroush@users.noreply.github.com>
2023-10-02 13:19:18 -0400

    Add new exercise file, working on VSC input

---
+++ c/variables-and-data/VariablesAndData.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int input_int;
+    int input_factor;
+
+    printf("Please type a number: ");
+    scanf("%d", &input_int);
+    /*
+    printf("Your inputted number was %d", input_int);
+    printf("Please type a number to multiply this number with: ");
+    scanf("%d", &input_factor);
+    printf("Your product is %d", input_int * input_factor);
+    printf("Thank you for your participation. Goodbye!");
+    */
+    return 0;
+}
