cacb828bdcaf57bc475db8f9cf058e4215e67ac1
Soren <sorensoroush@users.noreply.github.com>
2023-10-10 12:45:54 -0400

    Fix pointer reference from null pointer

--- c/pointers/Pointers.c
+++ c/pointers/Pointers.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-int num;
-int *numPointer;
-
 int main() {
-    printf("The current value for num: %d\n", num);
-    printf("The current value for numPointer: %d\n", numPointer);
-    // printf("The current value for numPointer's value: %d\n", *numPointer);
-    printf("Type an integer value for num: ");
-    scanf("%d", &num);
+    int num;
+    int *numPointer = &num;
+
+    printf("\n");
     printf("The current value for num: %d\n", num);
     printf("The current value for numPointer: %d\n", numPointer);
     printf("The current value for numPointer's value: %d\n", *numPointer);
+    printf("\n");
+    printf("Type an integer value for num: ");
+    scanf("%d", &num);
+    printf("\n");
+    printf("The current value for num: %d\n", num);
+    printf("The current value for numPointer: %d\n", numPointer);
+    printf("The current value for numPointer's value: %d\n", *numPointer);
+    printf("\n");
 }
