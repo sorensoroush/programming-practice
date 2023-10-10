6885241fa4c095c42e06ba76c3202b27d44e5f5b
Soren <sorensoroush@users.noreply.github.com>
2023-10-10 12:56:16 -0400

    Add introductory variable value test

--- c/variables-and-data/VariablesAndData.c
+++ c/variables-and-data/VariablesAndData.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 int main() {
-    int input_int;
-    int input_factor;
+    int inputInt;
+    int inputFactor;
+    int initializeValueTest;
 
+    printf("\n");
+    printf("We will begin with an introductory test\n");
+    printf("The value of an initial variable is %d\n", initializeValueTest);
+    printf("Attempting to increment this variable...\n");
+    initializeValueTest++;
+    printf("This variable's value is now %d\n", initializeValueTest);
+    printf("Now for your regularly scheduled programming (ha)...\n");
+    printf("\n");
     printf("Please type a number: ");
-    scanf("%d", &input_int);
-    printf("Your inputted number was %d\n", input_int);
+    scanf("%d", &inputInt);
+    printf("\n");
+    printf("Your inputted number was %d\n", inputInt);
+    printf("\n");
     printf("Please type a number to multiply this number with: ");
-    scanf("%d", &input_factor);
-    printf("Your product is %d\n", input_int * input_factor);
+    scanf("%d", &inputFactor);
+    printf("Your product is %d\n", inputInt * inputFactor);
+    printf("\n");
     printf("Thank you for your participation. Goodbye!\n");
+    printf("\n");
     return 0;
 }
