bf51150e2da63565973f1d9e90e2c4bafde059c6
Soren <sorensoroush@users.noreply.github.com>
2023-10-13 11:18:20 -0400

    Complete char pointer testing

--- c/pointers/PointersWithMoreTypes.c
+++ c/pointers/PointersWithMoreTypes.c
@@ -4,33 +4,41 @@ int main() {
     char *p = &c;
 
     printf("\n");
-    printf("Value of c: %d\n", c);
-    printf("Value of p: %d\n", p);
+    printf("Value of c (int): %d\n", c);
+    printf("Value of c (char): %c\n", c);
+    printf("Value of p (int): %d\n", p);
+    printf("Value of p (memory address): %p\n", p);
+    printf("Value of p (deference): %d\n", *p);
 
     printf("\n");
     printf("Please enter a single character value for c: ");
     scanf("%c", &c);
     printf("\n");
 
-    printf("Value of c: %d\n", c);
-    printf("Value of p: %d\n", p);
+    printf("Value of c (int): %d\n", c);
+    printf("Value of c (char): %c\n", c);
+    printf("Value of p (int): %d\n", p);
+    printf("Value of p (memory address): %p\n", p);
+    printf("Value of p (deference): %d\n", *p);
 
     printf("\n");
     printf("Adding 1 to the value of c...");
     printf("\n");
 
-    printf("Value of c: %d\n", c);
-    printf("Value of p: %d\n", p);
-    printf("Value of p's value: %d\n", *p);
+    printf("Value of c (int): %d\n", c);
+    printf("Value of c (char): %c\n", c);
+    printf("Value of p (int): %d\n", p);
+    printf("Value of p (memory address): %p\n", p);
+    printf("Value of p (deference): %d\n", *p);
 
     printf("\n");
     printf("Adding 1 to the value of p...");
     printf("\n");
 
-    printf("Value of c: %d\n", c);
-    printf("Value of p: %d\n", p);
-    printf("Value of p's value: %d\n", *p);
+    printf("Value of c (int): %d\n", c);
+    printf("Value of c (char): %c\n", c);
+    printf("Value of p (int): %d\n", p);
+    printf("Value of p (memory address): %p\n", p);
+    printf("Value of p (deference): %d\n", *p);
     printf("\n");
-
-    // I don't really understand other data types in C...
 }
