fe5f6d86f99287c6f6fd112feee619ca7c66cbe0
Soren <sorensoroush@users.noreply.github.com>
2023-10-22 21:19:52 -0400

    Add notation variations for values and addresses of array indices

--- c/arrays/ArrayTest.c
+++ c/arrays/ArrayTest.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    int nums[5];
+    int nums[5] = {5,2,6,8,1};
     printf("\n");
-    printf("Content of nums[0]: %d\n", nums[0]);
-    printf("Address of nums[0]: %a\n", &nums[0]);
-    printf("Content of nums[1]: %d\n", nums[1]);
-    printf("Address of nums[1]: %a\n", &nums[1]);
-    printf("Content of nums[2]: %d\n", nums[2]);
-    printf("Address of nums[2]: %a\n", &nums[2]);
-    printf("Content of nums[3]: %d\n", nums[3]);
-    printf("Address of nums[3]: %a\n", &nums[3]);
-    printf("Content of nums[4]: %d\n", nums[4]);
-    printf("Address of nums[4]: %a\n", &nums[4]);
+    printf("Content of nums[0] (nums[0]): %d\n", nums[0]);
+    printf("Content of nums[0] (*nums): %d\n", *nums);
+    printf("Address of nums[0] (nums): %d\n", nums);
+    printf("Address of nums[0] (&nums[0]): %d\n", &nums[0]);
+    printf("Content of nums[1] (nums[1]): %d\n", nums[1]);
+    printf("Content of nums[1] (*(nums + 1)): %d\n", *(nums + 1));
+    printf("Address of nums[1] (nums + 1): %d\n", nums + 1);
+    printf("Address of nums[1] (&nums[1]): %d\n", &nums[1]);
+    printf("Content of nums[2] (nums[2]): %d\n", nums[2]);
+    printf("Content of nums[2] (*(nums + 2)): %d\n", *(nums + 2));
+    printf("Address of nums[2] (nums + 2): %d\n", nums + 2);
+    printf("Address of nums[2] (&nums[2]): %d\n", &nums[2]);
+    printf("Content of nums[3] (nums[3]): %d\n", nums[3]);
+    printf("Content of nums[3] (*(nums + 3)): %d\n", *(nums + 3));
+    printf("Address of nums[3] (nums + 3): %d\n", nums + 3);
+    printf("Address of nums[3] (&nums[3]): %d\n", &nums[3]);
+    printf("Content of nums[4] (nums[4]): %d\n", nums[4]);
+    printf("Content of nums[4] (*(nums + 4)): %d\n", *(nums + 4));
+    printf("Address of nums[4] (nums + 4): %d\n", nums + 4);
+    printf("Address of nums[4] (&nums[4]): %d\n", &nums[4]);
     printf("\n");
 }
