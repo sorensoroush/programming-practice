3b43ccb0d615a8aabb4b7b06091a4f2969c1adf1
Soren <sorensoroush@users.noreply.github.com>
2023-11-11 12:17:41 -0500

    Change to incorporate realloc later, write writeArray

--- c/memory-allocation/CallocRealloc.c
+++ c/memory-allocation/CallocRealloc.c
@@ -2,39 +2,36 @@
 #include <stdlib.h>
 
 void printArray(int array[], int size);
+void writeArray(int arrayCopy[], int arrayPaste[], int size);
 
 int main() {
     int arraySize = 5;
+
+    printf("\n");
+    printf("Values to be written at mallocPointer address: [0,1,2,3,4]\n");
+    printf("Values to be written at callocPointer address: [5,6,7,8,9]\n");
+
     int mallocArray[] = {0,1,2,3,4};
     int callocArray[] = {5,6,7,8,9};
 
     int* mallocPointer = malloc(arraySize * sizeof(int));
-    int* callocPointer = calloc(arraySize, sizeof(int));
-    int* reallocPointer = realloc(mallocPointer, 2 * arraySize * sizeof(int));
+    int* callocPointer = calloc(5, sizeof(int));
 
     printf("\n");
     printf("Memory address of mallocPointer (&mallocPointer): %d\n", &mallocPointer);
     printf("Allocated memory address stored in mallocPointer (mallocPointer): %d\n", mallocPointer);
     printf("Contents of allocated memory address (*mallocPointer): ");
-    printArray(mallocPointer, arraySize + 1);
+    printArray(mallocPointer, arraySize);
     printf("\n");
 
     printf("\n");
     printf("Memory address of callocPointer (&callocPointer): %d\n", &callocPointer);
     printf("Allocated memory address stored in callocPointer (callocPointer): %d\n", callocPointer);
     printf("Contents of allocated memory address (*callocPointer): ");
-    printArray(callocPointer, arraySize + 2);
+    printArray(callocPointer, arraySize);
     printf("\n");
-    // Why are there six 0s? I only allocated 20 bytes, right?
+    // It seems calloc() zeroes out an extra byte, not sure why this happens
 
-    printf("\n");
-    printf("Memory address of reallocPointer (&reallocPointer): %d\n", &reallocPointer);
-    printf("Allocated memory address stored in reallocPointer (reallocPointer): %d\n", reallocPointer);
-    printf("Contents of allocated memory address (*reallocPointer): ");
-    printArray(reallocPointer, arraySize + 1);
-    printf("\n");
-
-    printf("\n");
     /*
     Initialize two arrays
     Specify these are values to be written, not array to copy
@@ -48,11 +45,20 @@ int main() {
     */
 }
 
-void printArray(int array[], int arraySize) {
-    printf("\n[TESTING] ARRAY SIZE: %d\n", arraySize);
-    int* arrayEnd = array + arraySize;
-    while (array < arrayEnd) {
-        printf("%d ", *array);
-        array++;
+void printArray(int* arrayStart, int size) {
+    // printf("\n[TESTING] ARRAY SIZE: %d\n", arraySize);
+    int* arrayEnd = arrayStart + size;
+    while (arrayStart < arrayEnd) {
+        printf("%d ", *arrayStart);
+        arrayStart++;
+    }
+}
+
+void writeArray(int* arrayCopy, int* arrayPaste, int size) {
+    int* arrayEnd = arrayCopy + size;
+    while (arrayCopy < arrayEnd) {
+        *arrayPaste = *arrayCopy;
+        arrayCopy++;
+        arrayPaste++;
     }
 }
