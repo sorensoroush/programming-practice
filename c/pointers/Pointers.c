2a69b3c86439c1c4efeba87e723145ea6c4c07fc
Soren <sorensoroush@users.noreply.github.com>
2023-11-16 13:22:02 -0500

    Reformat pointer demo for readability

--- c/pointers/Pointers.c
+++ c/pointers/Pointers.c
@@ -1,63 +1,62 @@
 #include <stdio.h>
 
 int main() {
-    /* 
-    Two variables are declared and tested here because
-    if only one variable is declared it will have a default value of 0
-    yet if two variables are declared they will have default values of random large integers
-    */
+
+    printf("---------------\n");
+    printf("\n");
 
     int num;
     int num2;
 
-    printf("\n");
-    printf("Value of num before assigning pointer: %d\n", num);
-    printf("Value of num2 before assigning pointer: %d\n", num2);
+    int* numPointer = &num;
+    int* num2Pointer = &num2;
 
-    printf("\n");
-    printf("int *numPointer = &num;\n");
-    printf("int *numPointer2 = &num2;\n");
 
-    int *numPointer = &num;
-    int *numPointer2 = &num2;
+    printf("num before assigning value (num): %d\n", num);
+    printf("num2 before assigning value (num2): %d\n", num2);
+    printf("\n");
 
-    printf("\n");
-    printf("The current value for num after pointing to it: %d\n", num);
-    printf("The current value for numPointer: %d\n", numPointer);
-    printf("The current value for numPointer via derefencing: %d\n", *numPointer);
-    printf("\n");
-    printf("The current value for num2 before pointing to it: %d\n", num2);
-    printf("The current value for numPointer2: %d\n", numPointer2);
-    printf("The current value for numPointer2 via derefencing: %d\n", *numPointer2);
-
-    printf("\n");
+    /*
     printf("Type an integer value for num: ");
     scanf("%d", &num);
     printf("Type an integer value for num2: ");
     scanf("%d", &num2);
+    */
+
+    num = 1;
+    num2 = 5;
+
+    printf("num after assigning value (num): %d\n", num);
+    printf("num2 after assigning value (num2): %d\n", num2);
+    printf("num's address (&num): %d\n", &num);
+    printf("num2's address (&num2): %d\n", &num2);
+    printf("numPointer (numPointer): %d\n", numPointer);
+    printf("num2Pointer (num2Pointer): %d\n", num2Pointer);
+    printf("numPointer's dereferenced value (*numPointer): %d\n", *numPointer);
+    printf("num2Pointer's dereferenced value (*num2Pointer): %d\n", *num2Pointer);
+    printf("numPointer's address (&numPointer): %d\n", &numPointer);
+    printf("num2Pointer's address (&num2Pointer): %d\n", &num2Pointer);
+    printf("\n");
 
     printf("\n");
-    printf("The current value for num: %d\n", num);
-    printf("The current value for numPointer: %d\n", numPointer);
-    printf("The current value for numPointer's dereferenced value: %d\n", *numPointer);
-    printf("The current value for num2: %d\n", num2);
-    printf("\n");
-    printf("The current value for numPointer2: %d\n", numPointer2);
-    printf("The current value for numPointer2's dereferenced value: %d\n", *numPointer2);
-
-    printf("\n");
-    printf("Attempting adding 1 to pointerNum...\n");
-    printf("Attempting adding 10 to pointerNum2...\n");
+    printf("Adding 1 to pointerNum...\n");
+    printf("Adding 10 to pointerNum2...\n");
 
     numPointer += 1;
-    numPointer2 += 10;
+    num2Pointer += 10;
 
     printf("\n");
-    printf("The current value for num: %d\n", num);
-    printf("The current value for num2: %d\n", num2);
-    printf("The current value for numPointer: %d\n", numPointer);
-    printf("The current value for numPointer2: %d\n", numPointer2);
-    printf("The current value for numPointer's value: %d\n", *numPointer);
-    printf("The current value for numPointer2's value: %d\n", *numPointer2);
+    printf("num (num): %d\n", num);
+    printf("num2 (num2): %d\n", num2);
+    printf("num's address (&num): %d\n", &num);
+    printf("num2's address (&num2): %d\n", &num2);
+    printf("numPointer (numPointer): %d\n", numPointer);
+    printf("num2Pointer (num2Pointer): %d\n", num2Pointer);
+    printf("numPointer's dereferenced value (*numPointer): %d\n", *numPointer);
+    printf("num2Pointer's dereferenced value (*num2Pointer): %d\n", *num2Pointer);
+    printf("numPointer's address (&numPointer): %d\n", &numPointer);
+    printf("num2Pointer's address (&num2Pointer): %d\n", &num2Pointer);
     printf("\n");
+
+    printf("---------------\n");
 }
