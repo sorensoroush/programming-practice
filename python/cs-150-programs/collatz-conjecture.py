143136466d4f0137620a7edb4468c272db9ddf85
sorensoroush <41019394+sorensoroush@users.noreply.github.com>
2024-10-17 09:32:57 -0400

    Add basic sequence function

--- python/cs-150-programs/collatz-conjecture.py
+++ python/cs-150-programs/collatz-conjecture.py
@@ -1,5 +1,24 @@
 def sequence():
-    print('Sequencing...')
+    print('')
+
+    input_integer = input('Please input a positive integer: ')
+
+    i = int(input_integer)
+
+    if (i > 0):
+        while (i > 1):
+            if (i % 2 == 1):
+                i = 3*i + 1
+            elif (i % 2 == 0):
+                i //= 2
+            else:
+                print('Oops, how did this happen? An integer with a modulus 2 result other than 0 or 1?')
+            
+            print(i)
+
+    else:
+        print('Sorry, that is not a positive integer')    
+
 
 def prime_range():
     print('Printing prime range...')
@@ -15,7 +34,7 @@ def main():
         case 'p':
             prime_range()
         case _:
-            print("I'm sorry, I didn't understand that")
+            print("\n" + "I'm sorry, I didn't understand that")
         
     print('')
 
