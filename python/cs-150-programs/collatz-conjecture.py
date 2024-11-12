155f0bb626d4be1608269eab26e20a5be39a4a6e
sorensoroush <41019394+sorensoroush@users.noreply.github.com>
2024-11-11 22:23:25 -0500

    Clean comments, consolidate sequence functions

--- python/cs-150-programs/collatz-conjecture.py
+++ python/cs-150-programs/collatz-conjecture.py
@@ -8,9 +8,11 @@ def prime_factors(num):
     dividend_sqrt = math.floor(math.sqrt(dividend))
     tested_prime = 2
 
+    # ---(Commentary)---
     # Upon successful divisor, reshift dividend and dividend_sqrt
     # If tested prime exceeds sqrt, it means the dividend is prime
     # Then terminate everything and add the dividend to the primes
+    # -----------------
 
     while tested_prime <= dividend_sqrt:
         if dividend % tested_prime == 0:
@@ -19,42 +21,41 @@ def prime_factors(num):
             dividend_sqrt = math.floor(math.sqrt(dividend))
         else:
             tested_prime += 1
+            # ? Is there a way to skip composites without calling another function?
     
     primes.append(dividend)
 
+    # ? Could somehow add libraries in the future to test performance?
+
     return primes
 
-def prime_range():
-    print('Printing prime range...')
-    print
-
-def sequence():
+def sequence(mode, num):
     print('')
 
     input_integer = input('Please input a positive integer: ')
 
-    i = int(input_integer)
+    num = int(input_integer)
 
-    if (i > 0):
-        while (i > 1):
-            if (i % 2 == 1):
-                i = 3*i + 1
-            elif (i % 2 == 0):
-                i //= 2
+    if (num > 0):
+        while (num > 1):
+            if (num % 2 == 1):
+                num = 3 * num + 1
+            elif (num % 2 == 0):
+                num //= 2
             else:
                 print('Oops, how did this happen? An integer with a modulus 2 result other than 0 or 1?')
             
-            print(i)
+            print(num)
 
     else:
         print('Sorry, that is not a positive integer')    
 
 def main():
 
-    selection_list = ['f', 'r', 's']
+    selection_list = ['f', 'p', 'd']
 
     print('')
-    mode = input('Please select a mode { prime (f)actors, prime (r)ange, Collatz (s)equence }: ')
+    mode = input('Please select a mode { prime (f)actors, (p)rimed sequence, (d)efault sequence }: ')
 
     if mode in selection_list:
         num_input = input('Please select a positive integer: ')
@@ -64,10 +65,10 @@ def main():
             case 'f':
                 primes = prime_factors(num)
                 print(primes)
-            case 'r':
-                prime_range(num)
-            case 's':
-                sequence(num)
+            case 'p':
+                sequence('primed', num)
+            case 'd':
+                sequence('default', num)
     else:
         print('Sorry, I didn\'t understand that')
 
