c16d9f406b2a52e799c53f2e6c8f3fa5b3328dd9
sorensoroush <41019394+sorensoroush@users.noreply.github.com>
2024-12-06 16:25:07 -0500

    Compare phi with fibonacci

--- python/small/phi-powers.py
+++ python/small/phi-powers.py
@@ -1,4 +1,8 @@
 phi = (1 + 5 ** 0.5) / 2
+f_list = [0,1]
 
 for i in range(2,11):
     print(f'{i}: {phi**i}')
+    f_list.append(f_list[-1] + f_list[-2])
+    print(f'f{i}: {f_list[-1]}')
+    print('\n')
